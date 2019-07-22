5570b11db2e2dedda03b31d9e3b65a9a52cb1413
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-07-22 23:06:06 +0100

    don't use navigator language

--- components/LanguageProvider.tsx
+++ components/LanguageProvider.tsx
@@ -1,7 +1,5 @@
 import { Component, createContext, Context } from "react";
 
-import { languages } from "../data/config.json";
-
 const DEFAULT_LANGUAGE: string = "en";
 
 const LanguageContext: Context<{
@@ -25,18 +23,7 @@ export default class LanguageProvider extends Component<
 > {
   constructor(props) {
     super(props);
-    if (navigator) {
-      const browserLanguage: string = (navigator.languages !== undefined
-        ? navigator.languages[0]
-        : navigator.language
-      ).slice(0, 2);
-      const initialLanguage: string = languages.includes(browserLanguage)
-        ? browserLanguage
-        : DEFAULT_LANGUAGE;
-      this.state = { language: initialLanguage };
-    } else {
-      this.state = { language: DEFAULT_LANGUAGE };
-    }
+    this.state = { language: DEFAULT_LANGUAGE };
     this.switchLanguage = this.switchLanguage.bind(this);
   }
   switchLanguage(language: string) {
